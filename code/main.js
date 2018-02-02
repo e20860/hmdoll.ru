b2e02516b6a5e67bbec14e7a76fed76b828f4227
Eugenie <e20860@mail.ru>
2018-02-02 16:50:37 +0700

    Заработало главное меню

--- code/main.js
+++ code/main.js
@@ -11,53 +11,155 @@ function main() {
 
   // Объявляем меню
   $menu.menu();
-  
-  // Слушатель главного меню
-  $menuItems.click(function () {
+
+  // Запоминаем пункты меню в переменных
+  $mn = $("#menu li:contains('Главная')");       //main
+  $pg = $("#menu li:contains('Фотогалерея')");   //photo gallery
+  $gl = $("#menu li:contains('Девочки')");       // girls
+  $by = $("#menu li:contains('Мальчики')");      // boys
+  $an = $("#menu li:contains('Зверушки')");      // animals
+  $pr = $("#menu li:contains('Процесс')");       // process
+  $pt = $("#menu li:contains('Выкройки')");      // patterns
+  $ct = $("#menu li:contains('Контакты')");      // contacts
+  // к переменным добавляем слушателей
+    
+  $mn.click(function () {
      var menuItem = $(this).text();
-     var $nt = $("#navitext");
-     $nt.text(menuItem);
      manageMenu(menuItem);
-  });  //#menu li
+  });  //
+  
+  $pg.click(function () {
+     var menuItem = $(this).text();
+     manageMenu(menuItem);
+  });  //
+  $gl.click(function () {
+     var menuItem = $(this).text();
+     manageMenu(menuItem);
+  });  //
+  $by.click(function () {
+     var menuItem = $(this).text();
+     manageMenu(menuItem);
+  });  //
+  $an.click(function () {
+     var menuItem = $(this).text();
+     manageMenu(menuItem);
+  });  //
+
+  $pr.click(function () {
+     var menuItem = $(this).text();
+     manageMenu(menuItem);
+  });  //
+  $pt.click(function () {
+     var menuItem = $(this).text();
+     manageMenu(menuItem);
+  });  //
+  $ct.click(function () {
+     var menuItem = $(this).text();
+     manageMenu(menuItem);
+  });  //
   
   
+  
+  // Формируем первую страницу с главным меню  
+  manageMenu("Главная"); 
+
+     
+   //********************************************************************************************  
   function manageMenu(menuItem) {
       // разруливаем главное меню взависимости от menuItem 
-      if ("МальчикиДевочкиЗверушки".indexOf(menuItem) != -1 ) return;  // Ничего в меню не меняем    
-
+     var $nt = $("#navitext");
+     $nt.text(menuItem);
+     if ("МальчикиДевочкиЗверушки".indexOf(menuItem) != -1 ) {
+         // Сразу к галерее, минуя перетурбации с меню
+        setGallery(menuItem);
+        return;
+     }     
+     // Убрать (деактивировать) все пункты меню
+     detachAll();
      switch(menuItem) { 
         case "Главная":
+           setMain();
            break;
         case "Фотогалерея":
+           $menu.append($mn);
+           $menu.append($gl);
+           $menu.append($by);
+           $menu.append($an);
+           $menu.append($pr);
+           $menu.append($pt);
+           $menu.append($ct);
+           setGallery("Девочки");
            break;
         case "Процесс":
+           setProcess();
            break;
         case "Выкройки":
+           setPatterns();
            break;
         case "Контакты":
-           $("article").empty();
+           setContacts();
            break;
      
      } //switch    
   }  // refillMenu
 
+  //---------------------------------------------------------------------------------------
+  function detachAll() {
+     $mn.detach();
+     $pg.detach();
+     $gl.detach();
+     $by.detach();
+     $an.detach();
+     $pr.detach();
+     $pt.detach();
+     $ct.detach();  
+  }
+
+  //--------------------------------------------------------------------------------------
   function setMain() {
+     $menu.append($pg);
+     $menu.append($pr);
+     $menu.append($pt);
+     $menu.append($ct);
      
   } // setMain
 
+//--------------------------------------------------------------------------------------
+
   function setGallery(galType) {
+
      
   } // setGallery
+
+//--------------------------------------------------------------------------------------
   
   function setProcess() {
+     $menu.append($mn);
+     $menu.append($pg);
+     $menu.append($pt);
+     $menu.append($ct);
      
   } // setProcess
+
+//--------------------------------------------------------------------------------------
   
   function setPatterns() {
+
+     $menu.append($mn);
+     $menu.append($pg);
+     $menu.append($pr);
+     $menu.append($ct);
      
   } // setPatterns
+
+//--------------------------------------------------------------------------------------
   
   function setContacts() {
+
+     $menu.append($mn);
+     $menu.append($pg);
+     $menu.append($pr);
+     $menu.append($pt);
      
   } // setContacts
    
